3b6a375e5ace35f1ae1b10babfd42e1a8c797bb4
Falgun Manishbhai Makadia <fl410364@dal.ca>
2021-02-17 20:27:00 +0530

    CICD Working Test 1 - Modified BankMain.java

--- A1/src/main/java/good/BankMain.java
+++ A1/src/main/java/good/BankMain.java
@@ -3,26 +3,26 @@ package good;
 public class BankMain {
 
 	public static void main(String[] args) {
-		
+
 		Banking_interest i = new Banking_interest();
 		Banking_homeLoan hl = new Banking_homeLoan();
 		Banking_educationLoan el = new Banking_educationLoan();
 		Banking_carLoan cl = new Banking_carLoan();
-		
-		double ciAmount = i.compoundInterestCalculator(500000, 7.8, 5);
-		System.out.println("Earned Compound Interest Amount:"+ciAmount);
-		
-		double fd = i.fixedDepositInterestCalculator(800000, 4);
-		System.out.println("Earned Fixed Deposit Interest:"+fd);
-		
-		double hl_emi = hl.homeLoanEmiCalculator(1400000, 15);
-		System.out.println("Home Loan EMI Amount:"+hl_emi);
-		
+
+		double ciAmount = i.compoundInterestCalculator(600000, 7.8, 5);
+		System.out.println("Earned Compound Interest Amount:" + ciAmount);
+
+		double fd = i.fixedDepositInterestCalculator(800000, 4.5);
+		System.out.println("Earned Fixed Deposit Interest:" + fd);
+
+		double hl_emi = hl.homeLoanEmiCalculator(1400000, 12);
+		System.out.println("Home Loan EMI Amount:" + hl_emi);
+
 		double el_emi = el.educationLoanEmiCalculator(750000, 2);
-		System.out.println("Education Loan EMI Amount:"+el_emi);
-		
-		double cl_emi = cl.carLoanEmiCalculator(500000, 3);
-		System.out.println("Car Loan EMI Amount:"+cl_emi);
+		System.out.println("Education Loan EMI Amount:" + el_emi);
+
+		double cl_emi = cl.carLoanEmiCalculator(600000, 5);
+		System.out.println("Car Loan EMI Amount:" + cl_emi);
 
 	}
 
